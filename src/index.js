f75c69a9830778daf128dbf3b6e89e3cc8f80eca
Rodrigo Chaljub <rodrian95@hotmail.com>
2019-06-03 21:44:22 -0500

    Añadir archivos react crud

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Example from "./example.js";
+ 
+ReactDOM.render(<Example />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+
